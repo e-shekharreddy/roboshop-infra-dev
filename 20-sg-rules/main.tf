c56b7e40f64043dd1c7a7ae1d88d94b82777c085
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-21 02:53:58 +0530

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -6,4 +6,38 @@ resource "aws_security_group_rule" "bastion_internet" {
 #   cidr_blocks       = ["0.0.0.0/0"]
   cidr_blocks       = [local.my_ip]
 # which security group you are creating this for "bastion"
-  security_group_id = data.aws_ssm_parameter.bastion_sg_id
+  security_group_id = local.bastion_sg_id
+}
+
+resource "aws_security_group_rule" "mongodb_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+# where traffic is comming from
+  source_security_group_id = local.bastion_sg_id
+# dest or accepting
+  security_group_id = local.mongodb_sg_id
+}
+
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+# where traffic is comming from
+  source_security_group_id = local.catalogue_sg_id
+# dest or accepting one
+  security_group_id = local.mongodb_sg_id
+}
+
+resource "aws_security_group_rule" "mongodb_user" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+# where traffic is comming from
+  source_security_group_id = local.user_sg_id
+# dest or accepting one
+  security_group_id = local.mongodb_sg_id
+}
