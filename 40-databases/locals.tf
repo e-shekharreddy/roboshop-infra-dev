29bcf752fbc1132c2654fec9de744c641424f265
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-21 22:46:44 +0530

    terraform

---
+++ 40-databases/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  common_tags = {
+        project = var.project 
+        environment = var.environment
+        terraform = "true"
+
+    }
+  ami_id = data.aws_ami.joindevops
+  #subnet in 1a AZ
+  database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_id.value)[0]
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+}
