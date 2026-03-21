c26d5041d6391682399e018e84e0ff33d3e65c7c
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-22 00:06:30 +0530

    terraform

--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -4,6 +4,8 @@ resource "aws_instance" "bastion" {
   subnet_id = local.public_subnet_id
   vpc_security_group_ids = [local.bastion_sg_id]
   iam_instance_profile = aws_iam_instance_profile.bastion.name
+  user_data = file("bastion.sh") # here file is function to read the content inside and give to ec2
+
   root_block_device {
     volume_size = 50
     volume_type = "gp3"
