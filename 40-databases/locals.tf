c9f9cccae28b14e49f57fc0e3a607ceca9bf43c0
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-23 15:28:56 +0530

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -7,7 +7,7 @@ locals {
     }
   ami_id = data.aws_ami.joindevops
   #subnet in 1a AZ
-  database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_id.value)[0]
+  database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_id).value[0]
   mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
   redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
