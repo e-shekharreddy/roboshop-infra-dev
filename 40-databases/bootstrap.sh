6c0388fad912b0850adc5562bce70ce2358bb3fd
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-23 16:49:18 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 component=$1
+environment=$2
 dnf install ansible -y
 
 cd /home/ec2-user
@@ -9,4 +10,4 @@ git clone https://github.com/e-shekharreddy/ansible-roboshop-roles-tf.git
 cd ansible-roboshop-roles-tf
 git pull
 
-ansible-playbook -e component=$component roboshop.yaml
+ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
