4484cac6c7b0457159103d6a963a6da7b04bd28c
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-23 14:51:42 +0530

    terraform

--- 30-bastion/locals.tf
+++ 30-bastion/locals.tf
@@ -5,7 +5,7 @@ locals {
         terraform = "true"
 
     }
-  ami_id = data.aws_ami.joindevops
+  ami_id = data.aws_ami.joindevops.id
   #subnet in 1a AZ
   public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
   bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
