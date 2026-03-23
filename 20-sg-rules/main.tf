95d539cc8f1ee6c5ca81d986be7cd5c46769c399
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-23 14:31:01 +0530

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -52,3 +52,25 @@ resource "aws_security_group_rule" "redis_bastion" {
 # dest or accepting
   security_group_id = local.redis_sg_id
 }
+
+resource "aws_security_group_rule" "mysql_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+# where traffic is comming from
+  source_security_group_id = local.bastion_sg_id
+# dest or accepting
+  security_group_id = local.mysql_sg_id
+}
+
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+# where traffic is comming from
+  source_security_group_id = local.bastion_sg_id
+# dest or accepting
+  security_group_id = local.rabbitmq_sg_id
+}
--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -7,5 +7,6 @@ cd /home/ec2-user
 git clone https://github.com/e-shekharreddy/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
+git pull
 
 ansible-playbook -e component=$component roboshop.yaml
