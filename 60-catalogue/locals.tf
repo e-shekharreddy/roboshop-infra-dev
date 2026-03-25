0b7bcaedfa25600d64d91f514ec44ee74287fa62
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-25 23:22:02 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -5,8 +5,8 @@ locals {
         environment = var.environment
         terraform = true
     }
-  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.id
+  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
-  ami_id = data.aws_ami.joindevops.value
+  ami_id = data.aws_ami.joindevops.id
   
 }
