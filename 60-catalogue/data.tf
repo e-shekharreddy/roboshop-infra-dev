d8dd5e616a1f78f21b9290fff44a81b412e953ee
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-25 21:58:49 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -1,9 +1,9 @@
 data "aws_ssm_parameter" "catalogue_sg_id" {
-  value = "/${var.project}/${var.environment}/catalogue_sg_id"
+  name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
 
 data "aws_ssm_parameter" "private_subnet_ids" {
-  value = "/${var.project}/${var.environment}/private_subnet_ids"
+  name = "/${var.project}/${var.environment}/private_subnet_ids"
 }
 
 data "aws_ami" "joindevops" {
