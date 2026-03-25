d87f0eb1386d424ac70a73fd302d0132f7ecbd8c
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-25 22:50:29 +0530

    terraform

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -3,7 +3,7 @@
 # we are creating 50Gb root disk, but only 20gb is partioned 
 # reamaing 30GB we need to exted using below commands
 growpart /dev/nvme0n1 4
-lvextend -r -L +30G /dev/mapper/RootVG-homeVo1
+lvextend -r -L +30G /dev/mapper/RootVG-homeVol
 xfs_growfs /home
 
 yum install -y yum-utils
