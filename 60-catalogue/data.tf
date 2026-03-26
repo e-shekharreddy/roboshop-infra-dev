9d54b99b643e68cee9a9164d228b80191b76780a
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-26 23:11:40 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -26,3 +26,14 @@ data "aws_ami" "joindevops" {
   }
 }
 
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project}/${var.environment}/vpc_id"
+}
+
+data "aws_ssm_parameter" "vpc_sg_id" {
+  name = "/${var.project}/${var.environment}/vpc_sg_id"
+}
+
+data "aws_ssm_parameter" "backend_alb_listener_arn" {
+  name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
+}
--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -8,5 +8,7 @@ locals {
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   ami_id = data.aws_ami.joindevops.id
-  
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  vpc_sg_id = data.aws_ssm_parameter.vpc_sg_id.id
+  backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
 }
