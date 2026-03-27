9727f1e2cf9872398c7ccde0cda33c043b1a1852
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-28 02:21:59 +0530

    terraform

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -1,7 +1,6 @@
 data "http" "my_public_ip" {
   url = "https://ipv4.icanhazip.com"
 }
-
 output "my_ip" {
   # Use chomp to remove the trailing newline character
   value = local.my_ip
@@ -14,40 +13,43 @@ data "aws_ssm_parameter" "bastion_sg_id" {
 data "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project}/${var.environment}/mongodb_sg_id"
 }
-
-
 data "aws_ssm_parameter" "redis_sg_id" {
   name = "/${var.project}/${var.environment}/redis_sg_id"
 }
-
 data "aws_ssm_parameter" "mysql_sg_id" {
   name = "/${var.project}/${var.environment}/mysql_sg_id"
 }
-
 data "aws_ssm_parameter" "rabbitmq_sg_id" {
   name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
 }
 
-
-
-
-
 data "aws_ssm_parameter" "catalogue_sg_id" {
   name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
-
 data "aws_ssm_parameter" "user_sg_id" {
   name = "/${var.project}/${var.environment}/user_sg_id"
 }
+data "aws_ssm_parameter" "cart_sg_id" {
+  name = "/${var.project}/${var.environment}/cart_sg_id"
+}
+data "aws_ssm_parameter" "shipping_sg_id" {
+  name = "/${var.project}/${var.environment}/shipping_sg_id"
+}
+data "aws_ssm_parameter" "payment_sg_id" {
+  name = "/${var.project}/${var.environment}/payment_sg_id"
+}
+
 
 data "aws_ssm_parameter" "backend_alb_sg_id" {
   name = "/${var.project}/${var.environment}/backend_alb_sg_id"
+}
+data "aws_ssm_parameter" "backend_sg_id" {
+  name = "/${var.project}/${var.environment}/backend_sg_id"
 }
 
 data "aws_ssm_parameter" "frontend_alb_sg_id" {
   name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
 }
-
 data "aws_ssm_parameter" "frontend_sg_id" {
   name = "/${var.project}/${var.environment}/frontend_sg_id"
 }
