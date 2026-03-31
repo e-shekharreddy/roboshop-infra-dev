48ef9db238dba3f284fb58245c4b699a5064fe80
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-31 13:51:31 +0530

    terraform

--- 45-components/main.tf
+++ 45-components/main.tf
@@ -1,7 +1,6 @@
 module "component" {
     for_each = var.components
-    source = "../../terraform-roboshop-component"
-    # source = "git::https://github.com/e-shekharreddy/terraform-roboshop-component.git?ref=main"
+    source = "git::https://github.com/e-shekharreddy/terraform-roboshop-component.git?ref=main"
     component = each.key
     rule_priority = each.value.rule_priority
 }
